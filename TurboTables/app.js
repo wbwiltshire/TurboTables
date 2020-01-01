dfd2d8485c2ffb4ba6741dd26196be61e4d93b08
wbwiltshire <wbwiltshire@hotmail.com>
2020-01-01 11:59:32 -0500

    Updated for VS 2019

--- TurboTables/app.js
+++ TurboTables/app.js
@@ -94,7 +94,7 @@ app.use(function (err, req, res, next) {
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(process.env.PORT || '3000');
+var port = normalizePort(process.env.PORT || '3001');
 app.set('port', port);
 
 //app.set('port', process.env.PORT);
