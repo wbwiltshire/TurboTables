5aa6647516596c1bee5cdaf02d579bf72157cb3b
wbwiltshire <wbwiltshire@hotmail.com>
2025-09-24 19:04:45 -0400

    Replace deprecated request package with axios

--- TurboTables/routes/pager.js
+++ TurboTables/routes/pager.js
@@ -1,8 +1,7 @@
 //Routing for pager web page
 var express = require('express');
 var router = express.Router();
-var request = require('request');
-require('request-debug')(request);
+const axios = require('axios');
 
 var customerRoute = '/api/customer/';
 var page = 1;
@@ -14,22 +13,30 @@ var filterValue = '';
 
 /* GET default route */
 /* http://localhost/pager */
-router.get('/', function (req, res, next) {
+router.get('/', async function (req, res, next) {
+     var response = null;
+     var json = null;
      //var options = setGetOptions('http://' + req.headers.host + customerRoute + '?page=' + page + '&pageSize=' + pageSize,
      var options = setGetOptions('http://' + req.headers.host + customerRoute + '?page=' + page + '&pageSize=' + pageSize + '&sortColumn=' + sortColumn + '&direction=' + sortDirection +
           '&filterColumn=' + filterColumn + '&filterValue' + filterValue,
           {}
      );
+
      //Call customer REST service
-     request(options, function (error, response, body) {
-          if (!error && response.statusCode === 200) {
-               var json = JSON.parse(body);
-               res.render('pager', { title: 'Paged Customer List', customers: json });
-          }
-          else {
-               console.log('Error: ' + response.statusCode + ' - ' + error);
-          }
-     });
+    try {
+        response = await axios.get(options.url);
+        json = response.data;
+        if (response.status === 200) {
+            var json = JSON.parse(body);
+            res.render('pager', { title: 'Paged Customer List', customers: json });
+        }
+        else
+            console.log('Error: ' + response.statusCode + ' - ' + error);
+    }
+    catch (error) {
+        console.log('Error: ', error);
+        next(error);     
+    }
 });
 
 function setGetOptions(u, q) {
